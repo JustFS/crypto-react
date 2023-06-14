f7be0f38facf8283026bd02eb6724379ea03e4cf
Julien Az <julien.azbrg@gmail.com>
2023-06-14 14:12:43 +0200

    added blue chip

--- src/components/Signal.js
+++ src/components/Signal.js
@@ -11,6 +11,7 @@ const Signal = ({ coin }) => {
   const [param, setParam] = useState(false);
   const [mini, setMini] = useState(0);
   const [maxi, setMaxi] = useState(0);
+  const [verif, setVerif] = useState(false);
   const [color, setColor] = useState("rgb(42, 42, 42)");
   const signalList = useSelector((state) => state.signalListReducer);
   const dispatch = useDispatch();
@@ -20,11 +21,12 @@ const Signal = ({ coin }) => {
       if (coin.signal === [0, 0]) {
         setColor("rgb(42, 42, 42)");
       }
-
+      console.log(signalList);
       for (let i = 0; i < signalList.length; i++) {
         if (signalList[i][0] === coin.id) {
           setMini(Number(signalList[i][1]));
           setMaxi(Number(signalList[i][2]));
+          setVerif(signalList[i][3]);
 
           if (coin.signal[0] < 0) {
             setColor("skyblue");
@@ -49,7 +51,7 @@ const Signal = ({ coin }) => {
   }, [param]);
 
   const setSignal = () => {
-    let data = [coin.id, mini, maxi];
+    let data = [coin.id, mini, maxi, verif];
 
     if (window.localStorage.signalData) {
       for (let i = 0; i < signalList.length; i++) {
@@ -72,9 +74,10 @@ const Signal = ({ coin }) => {
         if (coin.id === signalList[i][0]) {
           signalList.splice(i, 1);
           dispatch(setSignalList(signalList));
-          dispatch(updateCoinsData([coin.id, 0, 0]));
+          dispatch(updateCoinsData([coin.id, 0, 0, verif]));
           setMini(0);
           setMaxi(0);
+          setVerif(false);
           setColor("rgb(42, 42, 42)");
         }
       }
@@ -85,47 +88,55 @@ const Signal = ({ coin }) => {
     <div className="signal-container" id="signal-container">
       <div
         className="sign-color"
-        style={{ background: color }}
+        style={{
+          background: color,
+          outline: verif ? "3px solid #00b7b3" : null,
+        }}
         onClick={() => {
           setParam(!param);
         }}
       ></div>
 
       {param && (
-        <form
-          className="form"
-          onSubmit={(e) => {
-            e.preventDefault(e);
-            setSignal();
-            setParam(false);
-          }}
-        >
-          <input
-            type="text"
-            placeholder="support"
-            onChange={(e) => {
-              setMini(e.target.value);
+        <div class="signal-container">
+          <button onClick={() => setVerif(!verif)}>
+            {verif ? "Blue chip ?" : "Not blue chip ?"}
+          </button>
+          <form
+            className="form"
+            onSubmit={(e) => {
+              e.preventDefault(e);
+              setSignal();
+              setParam(false);
             }}
-            defaultValue={mini !== 0 ? mini : null}
-          />
-          <input
-            type="text"
-            placeholder="resistance"
-            onChange={(e) => setMaxi(e.target.value)}
-            defaultValue={maxi !== 0 ? maxi : null}
-          />
-          <div className="btn-container">
-            <div
-              onClick={() => {
-                deleteSignal();
-                setParam(false);
+          >
+            <input
+              type="text"
+              placeholder="support"
+              onChange={(e) => {
+                setMini(e.target.value);
               }}
-            >
-              <img src="./assets/delete-icon-bis.svg" alt="delete" />
+              defaultValue={mini !== 0 ? mini : null}
+            />
+            <input
+              type="text"
+              placeholder="resistance"
+              onChange={(e) => setMaxi(e.target.value)}
+              defaultValue={maxi !== 0 ? maxi : null}
+            />
+            <div className="btn-container">
+              <div
+                onClick={() => {
+                  deleteSignal();
+                  setParam(false);
+                }}
+              >
+                <img src="./assets/delete-icon-bis.svg" alt="delete" />
+              </div>
+              <input type="submit" value="Confirm" />
             </div>
-            <input type="submit" value="Confirm" />
-          </div>
-        </form>
+          </form>
+        </div>
       )}
     </div>
   );
