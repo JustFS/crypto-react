8bffb82c34bbfc36dd64f9f851f9ba2c8a57dd74
Julien Az <54361213+JustFS@users.noreply.github.com>
2025-08-09 13:18:21 +0200

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -11,7 +11,7 @@ import colors from "../styles/_settings.scss";
 
 const Table = () => {
   const [orderBy, setOrderBy] = useState("MarketCap");
-  const [rangeNumber, setRangeNumber] = useState(500);
+  const [rangeNumber, setRangeNumber] = useState(200);
   const [startNumber, setStartNumber] = useState(0);
   const [inputSearch, setInputSearch] = useState("");
   const [miniVol, setMiniVol] = useState(0);
